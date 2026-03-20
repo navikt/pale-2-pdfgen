8ad057b5a753d3e26fd5622985c0fe2f2338a05b
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2026-03-20 19:13:36 +0100

    Update Docker image version to 0.1.4

--- run_development.sh
+++ run_development.sh
@@ -2,7 +2,7 @@
 
 CURRENT_PATH="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
 
-docker pull ghcr.io/navikt/pdfgenrs:0.1.3
+docker pull ghcr.io/navikt/pdfgenrs:0.1.4
 docker run \
         -v $CURRENT_PATH/templates:/app/templates \
         -v $CURRENT_PATH/fonts:/app/fonts \
@@ -12,4 +12,4 @@ docker run \
         -e DISABLE_PDF_GET=false \
         -it \
         --rm \
-        ghcr.io/navikt/pdfgenrs:0.1.3
+        ghcr.io/navikt/pdfgenrs:0.1.4
