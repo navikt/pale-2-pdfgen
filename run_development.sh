95795fdbd52378ad6090690a30c33fddb23e1b26
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2026-03-24 17:22:04 +0100

    Update Docker image version in run_development.sh

--- run_development.sh
+++ run_development.sh
@@ -2,7 +2,7 @@
 
 CURRENT_PATH="$(cd "$(dirname "$1")"; pwd)/$(basename "$1")"
 
-docker pull ghcr.io/navikt/pdfgenrs:0.1.11
+docker pull ghcr.io/navikt/pdfgenrs:0.1.12
 docker run \
         -v $CURRENT_PATH/templates:/app/templates \
         -v $CURRENT_PATH/fonts:/app/fonts \
@@ -10,6 +10,7 @@ docker run \
         -v $CURRENT_PATH/resources:/app/resources \
         -v $CURRENT_PATH/resources:/app/templates/resources \
         -p 8080:8080 \
+        -e DEV_MODE=true \
         -it \
         --rm \
-        ghcr.io/navikt/pdfgenrs:0.1.11
+        ghcr.io/navikt/pdfgenrs:0.1.12
